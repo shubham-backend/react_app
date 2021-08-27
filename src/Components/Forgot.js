ee8aa4051d25293965a0ab63fef4647d1c252d28
shubham-backend <shubhamkr.gupta53@gmail.com>
2021-08-27 08:48:11 +0530

    Forgot Password

--- src/Components/Forgot.js
+++ src/Components/Forgot.js
@@ -1,8 +1,16 @@
+import {Link} from "react-router-dom"
 function Forgot()
 {
     return (
-        <div>
-            <h1>Forgot Page</h1>
+        <div className="Apps">
+            <form>
+            <b><i>Forgot Password</i></b><div className="form-group">
+                    <input class="form-control mr-sm-2" type="text" name="email" placeholder="Enater Email" aria-label="Search" />
+                    <div className="text-danger"></div>
+                </div>
+                <button class="btn btn-success my-2 my-sm-0" type="submit">Submit</button>
+                <p className="forgot" align="center"><Link to="/forgot">Back to Login</Link></p>
+            </form>
         </div>
     )
 }
